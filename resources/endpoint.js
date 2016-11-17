e98b823173eef22be239eff7d571102e7dfd3886
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2016-11-17 08:44:05 +0200

    webProxy support

--- resources/endpoint.js
+++ resources/endpoint.js
@@ -3,6 +3,15 @@
 // SteamCommunity URL's
 const SC  = 'http://steamcommunity.com/';
 const SCM = SC + 'market/';
+/**
+ * This is solely here because of SteamApis.com,
+ * it helps our ProxyAPI identify if the loaded page was indeed valid.
+ * Feel free to ignore this.
+ */
+const Contains = {
+    web: '33779068',
+    json: '"success"'
+};
 
 // Available endpoints
 module.exports = {
@@ -18,7 +27,10 @@ module.exports = {
             }).join('&');
         }
 
-        return `${SCM}search?${parameters}`;
+        return {
+            contains: Contains.web,
+            url: `${SCM}search?${parameters}`
+        };
     },
     /**
      * Search render API
@@ -44,42 +56,63 @@ module.exports = {
             }).join('&');
         }
 
-        return `${SCM}search/render?${parameters}`;
+        return {
+            contains: Contains.json,
+            url: `${SCM}search/render?${parameters}`
+        };
     },
     /**
      * Gets the popular listings
      */
     'popular': function(start, count) {
-        return SCM + `popular?language=english&currency=1&start=${start}&count=${count}`;
+        return {
+            contains: Contains.json,
+            url: `${SCM}popular?language=english&currency=1&start=${start}&count=${count}`
+        };
     },
     /**
      * Gets the recently created listings
      */
     'recent': function() {
-        return SCM + `recent?country=US&language=english&currency=1`;
+        return {
+            contans: Contans.json,
+            url: `${SCM}recent?country=US&language=english&currency=1`
+        };
     },
     /**
      * Gets the recently completed/sold listings
      */
     'recentcompleted': function() {
-        return SCM + `recentcompleted`;
+        return {
+            contains: Contains.json,
+            url: `${SCM}recentcompleted`
+        };
     },
     /**
      * Gets the listings page using appID and marketHashName
      */
     'listings': function(appID, marketHashName) {
-        return SCM + `listings/${appID}/${marketHashName}`;
+        return {
+            contains: Contains.web,
+            url: `${SCM}listings/${appID}/${marketHashName}`
+        };
     },
     /**
      * Gets the item activity on the listings page
      */
     'itemordersactivity': function(nameID) {
-        return SCM + `itemordersactivity?language=english&currency=1&item_nameid=${nameID}`;
+        return {
+            contains: Contains.json,
+            url: `${SCM}itemordersactivity?language=english&currency=1&item_nameid=${nameID}`
+        };
     },
     /**
      * Gets the item sale history on the listings page
      */
     'itemordershistogram': function(nameID) {
-        return SCM + `itemordershistogram?language=english&currency=1&item_nameid=${nameID}`;
+        return {
+            contains: Contains.json,
+            url: `${SCM}itemordershistogram?language=english&currency=1&item_nameid=${nameID}`
+        };
     }
 };

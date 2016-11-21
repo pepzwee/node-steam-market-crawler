33c776257d749cbe0c5e146635353f68b1690103
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2016-11-21 16:56:06 +0200

    Bug fixes

--- resources/endpoint.js
+++ resources/endpoint.js
@@ -78,7 +78,7 @@ module.exports = {
      */
     'recent': function() {
         return new function() {
-            this.contans = Contains.json;
+            this.contains = Contains.json;
             this.url = `${SCM}recent?country=US&language=english&currency=1`;
             this.base64 = new Buffer(this.url).toString('base64');
         };

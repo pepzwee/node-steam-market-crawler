35da4985720065954f487100b707724c832729a2
Kristjan Kirpu <kristjan.kirpu@gmail.com>
2016-11-23 22:46:04 +0200

    Gitignore & Dom comments

--- resources/dom.js
+++ resources/dom.js
@@ -56,7 +56,6 @@ module.exports = function($) {
             // Cheerio does not support ":last" and ":first" at the moment so we have to use .first() and .last()
             if(element.indexOf(':') !== -1) {
                 element = element.split(':');
-                console.log(element);
                 let selector = $(element[0])[element[1]](); // element[1] is either "first" or "last"
                 // Check if it exists and has any content
                 if(selector.length && selector.text().trim().length >= 1) {
@@ -146,33 +145,3 @@ module.exports = function($) {
 
     return dom;
 }
-/*
-exports.UserMarketListing = {
-    row: '.market_listing_row_link',
-    market_name: '.market_listing_item_name_link',
-    market_link: '.market_listing_item_name_link',
-    game_name: '.market_listing_game_name',
-    item_image: 'img.market_listing_item_img',
-
-    normal_price: '.market_listing_price_with_fee',
-    sale_price: '.market_listing_price_with_publisher_fee_only'
-};
-
-exports.MarketListing = {
-    row: '.market_listing_row_link',
-    market_name: '.market_listing_item_name',
-    game_name: '.market_listing_game_name',
-    item_image: 'img.market_listing_item_img',
-
-    quantity: '.market_listing_num_listings_qty',
-
-    normal_price: '.normal_price',
-    sale_price: '.sale_price'
-};
-
-exports.MarketItem = {
-    market_name: 'h1#largeiteminfo_item_name',
-    market_name_alt: '.market_listing_nav a',
-    image: '.market_listing_largeimage img'
-}
-*/
